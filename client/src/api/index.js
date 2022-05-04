b22ec7d71d5d270f6065d58c373fbfb2e1838ad9
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-05-04 14:14:09 -0400

    add env file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,5 +4,6 @@ const url = 'http://localhost:3001/api';
 
 const getHome = () => axios.get(url);
 const getMap = () => axios.get(`${url}/map`);
+const getAllCoins = () => axios.get(`${url}/coins`);
 
-export { getHome, getMap }
+export { getHome, getMap, getAllCoins }
