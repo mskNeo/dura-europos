34cdec4a4f8c80f20c48aefe7c68a1d005511bd4
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-05-08 16:52:24 -0400

    fleshed out api

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { getAllCoins } from '../controllers/dbController.js';
+import * as controller from '../controllers/dbController.js';
 
 const router = express.Router();
 
-router.get('/coins', getAllCoins);
+router.get('/coins', controller.getAllCoins);
+router.get('/coins/:coinNum', controller.getCoinByNumber);
+router.get('/coins/hoards/:hoardNum', controller.getCoinByHoard);
+router.get('/coins/emperors/:emperorName', controller.getCoinByEmperor);
+router.get('/coins/denomination/:denom', controller.getCoinByDenomination);
+router.get('/coins/mint/:mint', controller.getCoinByMint);
 
 export default router;
