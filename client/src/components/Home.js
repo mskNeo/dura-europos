16d8f6df9e6956d3eb2d87b45768adbf016d3531
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2022-05-09 02:03:44 -0400

    testing for data vis

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { getHome } from '../api';
+import { getHome, getAllCoins } from '../api';
 
 function Home() {
-    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('hi');
+    const [ emperors, setEmperors ] = useState({});
 
     useEffect(() => {
         getHome()
@@ -11,7 +12,19 @@ function Home() {
             console.log(data);
             setName(data.name);
             });
+        getAllCoins()
+            .then(res => {
+                let empObj = {};
+                res.data.forEach(coin => {
+                    empObj[coin.Emperor] = coin["Emperor Rule - End Date "];
+                });
+                setEmperors(empObj);
+            });
     }, []);
+
+    useEffect(() => {
+        console.log(Object.entries(emperors).sort((a, b) => new Date(a[1]) - new Date(b[1])));
+    }, [emperors]);
 
     return (
         <div>
